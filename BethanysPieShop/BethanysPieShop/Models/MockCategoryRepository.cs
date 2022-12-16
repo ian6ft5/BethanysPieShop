af3d5a8dc7988de0a95fe561956f5a817174fc28
ian6ft5 <irogers@bullfrogspas.com>
2022-12-16 15:52:38 -0700

    Module 4: added models and first controller

---
+++ BethanysPieShop/BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId = 1, CategoryName = "Fruit Pies", Description = "All-fruity pies"},
+                new Category{CategoryId = 2, CategoryName = "Cheesecakes", Description = "Cheesy all the way"},
+                new Category{CategoryId = 3, CategoryName = "Seasonal Pies", Description = "Get in the mood for a seasonal favorite"}
+            };
+    }
+}
