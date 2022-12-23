20817a5d2b22ded8d119a3f0d93808ed8a772fbd
ian6ft5 <irogers@bullfrogspas.com>
2022-12-23 14:06:05 -0700

    Complete Module 7

--- BethanysPieShop/BethanysPieShop/Program.cs
+++ BethanysPieShop/BethanysPieShop/Program.cs
@@ -6,12 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
 builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 app.UseStaticFiles();
